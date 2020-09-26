8c338289d38ff85b33b5679015941ed9b33276d7
Andy Sokolowski <44684409+aesokolowski@users.noreply.github.com>
2020-09-26 19:31:28 -0400

    added template switch for cases of 0, 1 or more arguments

--- src/menu.cpp
+++ src/menu.cpp
@@ -1,8 +1,21 @@
 #include "../include/menu.h"
 #include <iostream>
 
+const char *MSG_0_ARGS = "Please provide an argument.\n";
+const char *TOO_MANY_ARGS = "Too many arguments... for now...\n";
+
 void initial(int argc, char **argv)
 {
-  std::cout << "\nIf you can see this I remember how to use headers\n"
-            << std::endl;
+  switch (argc) {
+    case 1:
+      std::cout << MSG_0_ARGS << std::endl;
+      break;
+    case 2:
+      std::cout << "You entered: " << *(argv + 1) << ".\n" << std::endl;
+      break;
+    default:
+      std::cout << TOO_MANY_ARGS << std::endl;
+  }
+
+  return;
 }
