80064a21eb99015bd911e20c4a73a936e08788a0
aesokolowski <andysoko1217@gmail.com>
2020-10-25 22:11:54 -0400

    Testing: making sure the test only accepts arguments that would normally be sent to the function it's testing (is_current_valid)

--- test/icv_test.cpp
+++ test/icv_test.cpp
@@ -27,6 +27,43 @@ int main(int argc, char **argv)
 	return 1;
     }
 
+    //  make sure flag only consists of 1s and 0s
+    for (size_t i = 0; i < len; i++) {
+        if (c_fl[i] < 0x30 || c_fl[i] > 0x31) {
+            std::cerr << "Flag can only have 1s and 0s, i.e. string of bits." << std::endl;
+	    return 1;
+	}
+    }
+
+    //  make sure second arg is only one character long
+    char *c_ch = argv[2];
+    size_t ch_len = strlen(c_ch);
+    if (ch_len != 1) {
+        std::cerr << "Second argument must have string length of 1." << std::endl;
+	return 1;
+    }
+    
+    //  make sure current valid roman numeral (add more later as scaling up)
+    char ch = c_ch[0];
+    switch (ch) {
+	case 0x49:
+        case 0x69:
+        case 0x56:
+	case 0x76:
+	case 0x58:
+	case 0x78:
+	    /*** do nothing ***/
+	    break;
+        default:
+	    std::cerr << "Current valid list of characters for second argument"
+		      << ":\nI i V v X x" << std::endl;
+	    return false;
+    }
+
+    //  convert flag to int
+    
+    //  finally call function for test
+
     std::cout << "No errors!" << std::endl;
 
     return 0;
