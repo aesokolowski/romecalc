bf4db7a399fbca53d23e7e660340b091b16d6161
aesokolowski <andysoko1217@gmail.com>
2020-10-26 22:25:31 -0400

    testing: post-facto added a test for dec_to_rn, I already know it works but it was more of a trial run because I want to test rn_to_dec before plugging it in, so to speak.

--- scripts/x64test.ps1
+++ scripts/x64test.ps1
@@ -1,2 +1,3 @@
 g++ .\test\util_test.cpp .\src\utilities.cpp .\src\UserNum.cpp -o testutils
 g++ .\test\icv_test.cpp .\src\utilities.cpp -o testicv
+g++ .\test\convs_test.cpp .\src\converters.cpp .\src\UserNum.cpp .\src\declarations.cpp -o testconvs
--- test/convs_test.cpp
+++ test/convs_test.cpp
@@ -1,9 +1,25 @@
-#include "../include/constants.h"
 #include "../include/converters.h"
 #include "../include/UserNum.h"
 #include <iostream>
 
 int main(int argc, char **argv)
 {
+    if (argc == 1) {
+        std::cerr << "Provide a string to test." << std::endl;
+	return 1;
+    }
 
+    if (argc > 2) {
+        std::cerr << "Too many arguments." << std::endl;
+	return 1;
+    }
+
+    UserNum user_num(argv[1]);
+
+    std::cout << "Test 1: dec_to_rn(UserNum): ";
+    char *result = dec_to_rn(user_num);
+    std::cout << result << std::endl;
+    delete result; result = NULL;
+
+    return 0;
 }
