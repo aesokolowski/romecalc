996061a4b5e6058eb74a5ce540868951ddc9c2b5
aesokolowski <andysoko1217@gmail.com>
2020-10-29 02:25:48 -0400

    semi-feature: 1 - 399 in standard roman numerals gets properly translated back to normal 1 - 399, still need to check for false positives but the next step is to plug it into .\rocal

--- scripts/xvi_1-399.ps1
+++ scripts/xvi_1-399.ps1
@@ -1,11 +1,11 @@
 if (-not (Test-Path -Path .\to1-399.txt)) {
-    New-Item -Path .\to1-399.txt | Out-Null
+  New-Item -Path .\to1-399.txt | Out-Null
 }
 
 $fn1 = ".\to1-399.txt"
 Set-Content -Path $fn1 -Value "Testing check_xvi for 1 - 399:"
 for ($i = 1; $i -lt 400; $i++) {
-    $rn = (.\rocal $i)
-    Add-Content -Path $fn1 -Value $rn
-    Add-Content -Path $fn1 -Value (.\testutils $rn)
+  $rn = (.\rocal $i)
+  Add-Content -Path $fn1 -Value $rn
+  Add-Content -Path $fn1 -Value (.\testutils $rn)
 }
