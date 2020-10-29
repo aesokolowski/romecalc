b0ee7be23323d2b9229cdbf362a197b13ee91053
aesokolowski <andysoko1217@gmail.com>
2020-10-29 02:57:38 -0400

    minor changes to test scripts labeled 1-399, doesn't change functionality, just using variables smarter

--- scripts/conv_xvi_1-399.ps1
+++ scripts/conv_xvi_1-399.ps1
@@ -1,11 +1,12 @@
-if (-not (Test-Path -Path .\co1-399.txt)) {
-  New-Item -Path .\co1-399.txt | Out-Null 
+$fn1 = ".\co1-399.txt"
+
+if (-not (Test-Path -Path $fn1)) {
+  New-Item -Path $fn1 | Out-Null 
 }
 
-$fn1 = ".\co1-399.txt"
 Set-Content -Path $fn1 -Value "Testing the converters for 1 - 399:"
 for ($i = 1; $i -lt 400; $i++) {
-    $rn = (.\rocal $i);
-    Add-Content -Path $fn1 -Value $rn
-    Add-Content -Path $fn1 -Value (.\testconvs $rn);
+  $rn = (.\rocal $i);
+  Add-Content -Path $fn1 -Value $rn
+  Add-Content -Path $fn1 -Value (.\testconvs $rn);
 }
--- scripts/xvi_1-399.ps1
+++ scripts/xvi_1-399.ps1
@@ -1,8 +1,9 @@
-if (-not (Test-Path -Path .\to1-399.txt)) {
-  New-Item -Path .\to1-399.txt | Out-Null
+$fn1 = ".\to1-399.txt"
+
+if (-not (Test-Path -Path $fn1)) {
+  New-Item -Path $fn1 | Out-Null
 }
 
-$fn1 = ".\to1-399.txt"
 Set-Content -Path $fn1 -Value "Testing check_xvi for 1 - 399:"
 for ($i = 1; $i -lt 400; $i++) {
   $rn = (.\rocal $i)
